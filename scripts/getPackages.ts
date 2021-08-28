05d881b5366a92b20ac621c73e0ba4a99157f288
Timo Glastra <timo@animo.id>
2021-08-28 18:02:57 +0200

    build: set up package finder
    Signed-off-by: Timo Glastra <timo@animo.id>

---
+++ scripts/getPackages.ts
@@ -0,0 +1,7 @@
+import { readdirSync } from 'fs'
+import path from 'path'
+
+const packages = readdirSync(path.join(__dirname, '..', 'packages'))
+
+console.log(`packages: ${JSON.stringify(packages)}`)
+console.log(`::set-output name=packages::${JSON.stringify(packages)}`)
