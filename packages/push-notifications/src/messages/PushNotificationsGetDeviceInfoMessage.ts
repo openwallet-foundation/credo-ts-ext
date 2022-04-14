4378ca6bbcab5369f9b49917707417b32d1cf443
Timo Glastra <timo@animo.id>
2022-04-14 14:57:34 +0200

    fix(push-notifications): add @id to messages
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/push-notifications/src/messages/PushNotificationsGetDeviceInfoMessage.ts
+++ packages/push-notifications/src/messages/PushNotificationsGetDeviceInfoMessage.ts
@@ -1,6 +1,10 @@
 import { AgentMessage } from '@aries-framework/core'
 import { Equals } from 'class-validator'
 
+interface PushNotificationsGetDeviceInfoOptions {
+  id?: string
+}
+
 /**
  * Message to get the device information from another agent for push notifications
  *
@@ -10,4 +14,12 @@ export class PushNotificationsGetDeviceInfoMessage extends AgentMessage {
   @Equals(PushNotificationsGetDeviceInfoMessage.type)
   public readonly type = PushNotificationsGetDeviceInfoMessage.type
   public static readonly type = 'https://didcomm.org/push-notifications-native/1.0/get-device-info'
+
+  public constructor(options: PushNotificationsGetDeviceInfoOptions) {
+    super()
+
+    if (options) {
+      this.id = options.id ?? this.generateId()
+    }
+  }
 }
