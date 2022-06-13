6c32ef3946a1ffd564da5765b31ccb44e972336d
Akiff Manji <akiff.manji@gmail.com>
2022-06-13 08:51:26 -0700

    Upgrades AFJ to latest 0.2.0. Fix credential endpoints type errors
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>
    Signed-off-by: Jan <60812202+janrtvld@users.noreply.github.com>

--- packages/rest/src/controllers/credentials/CredentialController.ts
+++ packages/rest/src/controllers/credentials/CredentialController.ts
@@ -1,5 +1,5 @@
-import { IndySdkError, Agent, RecordNotFoundError } from '@aries-framework/core'
-import { JsonEncoder } from '@aries-framework/core/build/utils/JsonEncoder'
+import { IndySdkError, Agent, RecordNotFoundError, CredentialProtocolVersion } from '@aries-framework/core'
+// import { JsonEncoder } from '@aries-framework/core/build/utils/JsonEncoder'
 import { isIndyError } from '@aries-framework/core/build/utils/indyError'
 import {
   Get,
@@ -14,9 +14,9 @@ import {
 } from 'routing-controllers'
 import { injectable } from 'tsyringe'
 
-import { AcceptCredentialProposalRequest } from '../../schemas/AcceptCredentialProposalRequest'
+// import { AcceptCredentialProposalRequest } from '../../schemas/AcceptCredentialProposalRequest'
 import { CredentialOfferRequest } from '../../schemas/CredentialOfferRequest'
-import { CredentialOfferTemp } from '../../schemas/CredentialOfferTemplate'
+// import { CredentialOfferTemp } from '../../schemas/CredentialOfferTemplate'
 import { CredentialProposalRequest } from '../../schemas/CredentialProposalRequest'
 
 @JsonController('/credentials')
@@ -29,24 +29,29 @@ export class CredentialController {
   }
 
   /**
-   * Retrieve credential record by credentialId
+   * Retrieve credential exchange record by credential record id
+   *
+   * @param credentialRecordId
+   * @returns CredentialExchangeRecord
    */
-  @Get('/:credentialId')
-  public async getCredentialById(@Param('credentialId') credentialId: string) {
+  @Get('/:credentialRecordId')
+  public async getCredentialById(@Param('credentialRecordId') credentialRecordId: string) {
     try {
-      const credential = await this.agent.credentials.getById(credentialId)
+      const credential = await this.agent.credentials.getById(credentialRecordId)
 
       return credential.toJSON()
     } catch (error) {
       if (error instanceof RecordNotFoundError) {
-        throw new NotFoundError(`credential with credentialId "${credentialId}" not found.`)
+        throw new NotFoundError(`credential with credential record id "${credentialRecordId}" not found.`)
       }
       throw new InternalServerError(`something went wrong: ${error}`)
     }
   }
 
   /**
-   * Retrieve all credential records
+   * Retrieve all credential exchange records
+   *
+   * @returns CredentialExchangeRecord[]
    */
   @Get('/')
   public async getAllCredentials() {
@@ -55,8 +60,12 @@ export class CredentialController {
   }
 
   /**
+   * TODO: Update CredentialProposalRequest
    * Initiate a new credential exchange as holder by sending a credential proposal message
    * to the connection with the specified connection id.
+   *
+   * @param proposal
+   * @returns CredentialExchangeRecord
    */
   @Post('/propose-credential')
   public async proposeCredential(
@@ -65,7 +74,10 @@ export class CredentialController {
   ) {
     const { connectionId, ...proposalRequest } = proposal
     try {
-      const credential = await this.agent.credentials.proposeCredential(connectionId, proposalRequest)
+      const credential = await this.agent.credentials.proposeCredential({
+        connectionId,
+        credentialFormats: { indy: proposalRequest },
+      })
 
       return credential.toJSON()
     } catch (error) {
@@ -77,48 +89,63 @@ export class CredentialController {
   }
 
   /**
+   * TODO: Update CredentialProposalRequest
    * Accept a credential proposal as issuer (by sending a credential offer message) to the connection
-   * associated with the credential record.
+   * associated with the credential exchange record.
+   *
+   * @param credentialRecordId
+   * @param proposal
+   * @returns CredentialExchangeRecord
    */
-  @Post('/:credentialId/accept-proposal')
+  @Post('/:credentialRecordId/accept-proposal')
   public async acceptProposal(
-    @Param('credentialId') credentialId: string,
+    @Param('credentialRecordId') credentialRecordId: string,
     @Body()
-    proposal: AcceptCredentialProposalRequest
+    proposal: CredentialProposalRequest
   ) {
     try {
-      const credential = await this.agent.credentials.acceptProposal(credentialId, proposal)
+      const { ...proposalRequest } = proposal.credentialProposal
+
+      const credential = await this.agent.credentials.acceptProposal({
+        credentialRecordId,
+        credentialFormats: { indy: proposalRequest },
+      })
 
       return credential.toJSON()
     } catch (error) {
       if (error instanceof RecordNotFoundError) {
-        throw new NotFoundError(`Credential with credential id "${credentialId}" not found.`)
+        throw new NotFoundError(`Credential with credential record id "${credentialRecordId}" not found.`)
       }
       throw new InternalServerError(`Something went wrong: ${error}`)
     }
   }
 
   /**
+   * DEPRECATED: All offers are now out of band
    * Creates a credential offer not bound to any existing connection
    */
-  @Post('/offer-outofband-credential')
-  public async offerCredentialOutOfBand(
-    @Body()
-    offer: CredentialOfferTemp
-  ) {
-    const credential = await this.agent.credentials.createOutOfBandOffer(offer)
+  // @Post('/offer-outofband-credential')
+  // public async offerCredentialOutOfBand(
+  //   @Body()
+  //   offer: CredentialOfferTemp
+  // ) {
+  //   const credential = await this.agent.credentials.createOutOfBandOffer(offer)
 
-    return {
-      message: `${this.agent.config.endpoints[0]}/?d_m=${JsonEncoder.toBase64URL(
-        credential.offerMessage.toJSON({ useLegacyDidSovPrefix: this.agent.config.useLegacyDidSovPrefix })
-      )}`,
-      credentialRecord: credential.credentialRecord,
-    }
-  }
+  //   return {
+  //     message: `${this.agent.config.endpoints[0]}/?d_m=${JsonEncoder.toBase64URL(
+  //       credential.offerMessage.toJSON({ useLegacyDidSovPrefix: this.agent.config.useLegacyDidSovPrefix })
+  //     )}`,
+  //     credentialRecord: credential.credentialRecord,
+  //   }
+  // }
 
   /**
+   * TODO: Update CredentiaOfferRequest
    * Initiate a new credential exchange as issuer by sending a credential offer message
    * to the connection with the specified connection id.
+   *
+   * @param offer
+   * @returns CredentialExchangeRecord
    */
   @Post('/offer-credential')
   public async offerCredential(
@@ -127,7 +154,11 @@ export class CredentialController {
   ) {
     const { connectionId, ...offerRequest } = offer
     try {
-      const credential = await this.agent.credentials.offerCredential(connectionId, offerRequest)
+      const credential = await this.agent.credentials.offerCredential({
+        connectionId,
+        protocolVersion: CredentialProtocolVersion.V1,
+        credentialFormats: { indy: offerRequest.preview },
+      })
 
       return credential.toJSON()
     } catch (error) {
@@ -147,17 +178,20 @@ export class CredentialController {
 
   /**
    * Accept a credential offer as holder (by sending a credential request message) to the connection
-   * associated with the credential record.
+   * associated with the credential exchange record.
+   *
+   * @param credentialRecordId
+   * @returns CredentialExchangeRecord
    */
-  @Post('/:credentialId/accept-offer')
-  public async acceptOffer(@Param('credentialId') credentialId: string) {
+  @Post('/:credentialRecordId/accept-offer')
+  public async acceptOffer(@Param('credentialRecordId') credentialRecordId: string) {
     try {
-      const credential = await this.agent.credentials.acceptOffer(credentialId)
+      const credential = await this.agent.credentials.acceptOffer({ credentialRecordId })
 
       return credential.toJSON()
     } catch (error) {
       if (error instanceof RecordNotFoundError) {
-        throw new NotFoundError(`Credential with credential id "${credentialId}" not found.`)
+        throw new NotFoundError(`Credential with credential record id "${credentialRecordId}" not found.`)
       }
       throw new InternalServerError(`Something went wrong: ${error}`)
     }
@@ -165,17 +199,20 @@ export class CredentialController {
 
   /**
    * Accept a credential request as issuer (by sending a credential message) to the connection
-   * associated with the credential record.
+   * associated with the credential exchange record.
+   *
+   * @param credentialRecordId
+   * @returns CredentialExchangeRecord
    */
-  @Post('/:credentialId/accept-request')
-  public async acceptRequest(@Param('credentialId') credentialId: string) {
+  @Post('/:credentialRecordId/accept-request')
+  public async acceptRequest(@Param('credentialRecordId') credentialRecordId: string) {
     try {
-      const credential = await this.agent.credentials.acceptRequest(credentialId)
+      const credential = await this.agent.credentials.acceptRequest({ credentialRecordId })
 
       return credential.toJSON()
     } catch (error) {
       if (error instanceof RecordNotFoundError) {
-        throw new NotFoundError(`Credential with credential id "${credentialId}" not found.`)
+        throw new NotFoundError(`Credential with credential record id "${credentialRecordId}" not found.`)
       }
       throw new InternalServerError(`Something went wrong: ${error}`)
     }
@@ -183,33 +220,38 @@ export class CredentialController {
 
   /**
    * Accept a credential as holder (by sending a credential acknowledgement message) to the connection
-   * associated with the credential record.
+   * associated with the credential exchange record.
+   *
+   * @param credentialRecordId
+   * @returns CredentialExchangeRecord
    */
-  @Post('/:credentialId/accept-credential')
-  public async acceptCredential(@Param('credentialId') credentialId: string) {
+  @Post('/:credentialRecordId/accept-credential')
+  public async acceptCredential(@Param('credentialRecordId') credentialRecordId: string) {
     try {
-      const credential = await this.agent.credentials.acceptCredential(credentialId)
+      const credential = await this.agent.credentials.acceptCredential(credentialRecordId)
 
       return credential.toJSON()
     } catch (error) {
       if (error instanceof RecordNotFoundError) {
-        throw new NotFoundError(`Credential with credential id "${credentialId}" not found.`)
+        throw new NotFoundError(`Credential with credential record id "${credentialRecordId}" not found.`)
       }
       throw new InternalServerError(`Something went wrong: ${error}`)
     }
   }
 
   /**
-   * Deletes a credentialRecord in the credential repository.
+   * Deletes a credential exchange record in the credential repository.
+   *
+   * @param credentialRecordId
    */
-  @Delete('/:credentialId')
+  @Delete('/:credentialRecordId')
   @OnUndefined(204)
-  public async deleteCredential(@Param('credentialId') credentialId: string): Promise<void> {
+  public async deleteCredential(@Param('credentialRecordId') credentialRecordId: string): Promise<void> {
     try {
-      await this.agent.credentials.deleteById(credentialId)
+      await this.agent.credentials.deleteById(credentialRecordId)
     } catch (error) {
       if (error instanceof RecordNotFoundError) {
-        throw new NotFoundError(`Credential with credential id "${credentialId}" not found.`)
+        throw new NotFoundError(`Credential with credential record id "${credentialRecordId}" not found.`)
       }
       throw new InternalServerError(`Something went wrong: ${error}`)
     }
--- packages/rest/src/schemas/CredentialOfferTemplate.ts
+++ packages/rest/src/schemas/CredentialOfferTemplate.ts
@@ -1,12 +1,10 @@
-import type { CredentialOfferTemplate } from '@aries-framework/core'
-
-import { CredentialPreview, AutoAcceptCredential } from '@aries-framework/core'
+import { V1CredentialPreview as CredentialPreview, AutoAcceptCredential } from '@aries-framework/core'
 import { Attachment } from '@aries-framework/core/build/decorators/attachment/Attachment'
 import { LinkedAttachment } from '@aries-framework/core/build/utils/LinkedAttachment'
 import { Type } from 'class-transformer'
 import { IsString, IsOptional, ValidateNested, IsEnum, Matches, IsInstance } from 'class-validator'
 
-export class CredentialOfferTemp implements CredentialOfferTemplate {
+export class CredentialOfferTemp {
   @IsString()
   @Matches(/^([a-zA-Z0-9]{21,22}):3:CL:(([1-9][0-9]*)|([a-zA-Z0-9]{21,22}:2:.+:[0-9.]+)):(.+)?$/)
   public credentialDefinitionId!: string
