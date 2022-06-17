118184220642c9b81e3807ea4e5eeb9ef0d4654d
Jan <60812202+janrtvld@users.noreply.github.com>
2022-06-17 12:31:44 +0200

    feat: started tsoa implementation

--- packages/rest/src/server.ts
+++ packages/rest/src/server.ts
@@ -1,42 +1,27 @@
 import 'reflect-metadata'
 import type { ServerConfig } from './utils/ServerConfig'
-import type { Express } from 'express'
+import type { Response as ExResponse, Request as ExRequest, NextFunction } from 'express'
 
 import { Agent } from '@aries-framework/core'
-import { validationMetadatasToSchemas } from 'class-validator-jsonschema'
-import { createExpressServer, getMetadataArgsStorage, useContainer, useExpressServer } from 'routing-controllers'
-import { routingControllersToSpec } from 'routing-controllers-openapi'
-import * as swaggerUiExpress from 'swagger-ui-express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import express from 'express'
+import { serve, generateHTML } from 'swagger-ui-express'
+import { ValidateError } from 'tsoa'
 import { container } from 'tsyringe'
 
 import { basicMessageEvents } from './events/BasicMessageEvents'
 import { connectionEvents } from './events/ConnectionEvents'
 import { credentialEvents } from './events/CredentialEvents'
 import { proofEvents } from './events/ProofEvents'
-import TsyringeAdapter from './utils/TsyringeAdapter'
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const packageJson = require('../package.json')
+import { RegisterRoutes } from './routes/routes'
 
 export const setupServer = async (agent: Agent, config: ServerConfig) => {
   container.registerInstance(Agent, agent)
-  useContainer(new TsyringeAdapter(container))
 
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  const controllers = [__dirname + '/controllers/**/*.ts', __dirname + '/controllers/**/*.js']
-
-  let server: Express
-
-  if (config.app) {
-    server = useExpressServer(config.app, {
-      controllers: controllers,
-    })
-  } else {
-    server = createExpressServer({
-      controllers: controllers,
-      cors: config.cors ?? true,
-    })
-  }
+  let server = express()
+  if (config.app) server = config.app
+  if (config.cors) server.use(cors())
 
   if (config.webhookUrl) {
     basicMessageEvents(agent, config)
@@ -45,27 +30,39 @@ export const setupServer = async (agent: Agent, config: ServerConfig) => {
     proofEvents(agent, config)
   }
 
-  const schemas = validationMetadatasToSchemas({
-    refPointerPrefix: '#/components/schemas/',
+  // Use body parser to read sent json payloads
+  server.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  )
+  server.use(bodyParser.json())
+  server.use('/docs', serve, async (_req: ExRequest, res: ExResponse) => {
+    return res.send(generateHTML(await import('./routes/swagger.json')))
   })
 
-  const storage = getMetadataArgsStorage()
-  const spec = routingControllersToSpec(storage, undefined, {
-    components: {
-      schemas,
-    },
-    info: {
-      description: packageJson.description,
-      title: agent.config.label,
-      version: packageJson.version,
-    },
+  RegisterRoutes(server)
+
+  server.use(function errorHandler(err: unknown, res: ExResponse, next: NextFunction): ExResponse | void {
+    if (err instanceof ValidateError) {
+      return res.status(422).json({
+        message: 'Validation Failed',
+        details: err?.fields,
+      })
+    }
+    if (err instanceof Error) {
+      agent.config.logger.error('Internal Server Error.', err)
+      return res.status(500).json({
+        message: 'Internal Server Error. Check server logging.',
+      })
+    }
+    next()
   })
 
-  server.use('/docs', swaggerUiExpress.serve, swaggerUiExpress.setup(spec))
-
-  server.get('/', (_req, res) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.json(spec)
+  server.use(function notFoundHandler(_req, res: ExResponse) {
+    res.status(404).send({
+      message: 'Not Found',
+    })
   })
 
   return server
