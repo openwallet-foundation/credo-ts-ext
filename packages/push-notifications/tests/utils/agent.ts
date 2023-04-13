af8b2f521e2f2b58fa6423245cc17dba5751a69b
Moriarty <moritz@animo.id>
2023-04-13 17:02:22 +0200

    chore: wip add missing deps and test setup
    Signed-off-by: Moriarty <moritz@animo.id>

--- packages/push-notifications/tests/utils/agent.ts
+++ packages/push-notifications/tests/utils/agent.ts
@@ -1,9 +1,35 @@
 import type { InitConfig } from '@aries-framework/core'
 
-import { Agent, ConsoleLogger, HttpOutboundTransport, LogLevel, WsOutboundTransport } from '@aries-framework/core'
+import {
+  Agent,
+  ConsoleLogger,
+  DidsModule,
+  HttpOutboundTransport,
+  LogLevel,
+  WsOutboundTransport,
+} from '@aries-framework/core'
+import {
+  IndySdkIndyDidRegistrar,
+  IndySdkIndyDidResolver,
+  IndySdkModule,
+  IndySdkSovDidResolver,
+} from '@aries-framework/indy-sdk'
 import { agentDependencies } from '@aries-framework/node'
+import indySdk from 'indy-sdk-react-native'
+
+import { indyNetworks } from './genesis'
 
 export const setupAgent = ({ name, publicDidSeed }: { name: string; publicDidSeed: string }) => {
+  const modules = {
+    indySdk: new IndySdkModule({
+      indySdk,
+      networks: indyNetworks,
+    }),
+    dids: new DidsModule({
+      registrars: [new IndySdkIndyDidRegistrar()],
+      resolvers: [new IndySdkSovDidResolver(), new IndySdkIndyDidResolver()],
+    }),
+  }
   const agentConfig: InitConfig = {
     label: name,
     walletConfig: {
@@ -16,7 +42,7 @@ export const setupAgent = ({ name, publicDidSeed }: { name: string; publicDidSee
   const agent = new Agent({
     config: agentConfig,
     dependencies: agentDependencies,
-    modules: undefined,
+    modules: modules,
   })
 
   agent.registerOutboundTransport(new WsOutboundTransport())
