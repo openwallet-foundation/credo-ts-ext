8632b74f0ddd09066c57bba260e129f48e0c2eb7
Berend Sliedrecht <blu3beri@proton.me>
2023-09-18 09:41:55 +0200

    fix: loosen types on ble inbound and outbound transport and session
    Signed-off-by: Berend Sliedrecht <blu3beri@proton.me>

--- packages/transport-ble/src/transports/BleTransportSession.ts
+++ packages/transport-ble/src/transports/BleTransportSession.ts
@@ -1,28 +1,29 @@
-import type { Central } from '@animo-id/react-native-ble-didcomm'
-import type { Agent, AgentContext, EncryptedMessage, TransportSession } from '@aries-framework/core'
+import type { Ble } from '@animo-id/react-native-ble-didcomm'
+import type { AgentContext, EncryptedMessage, TransportSession } from '@aries-framework/core'
 
 import { utils } from '@aries-framework/core'
 
 export class BleTransportSession implements TransportSession {
   public readonly type = 'ble'
   public id: string
-  private agent: Agent
-  private central: Central
 
-  public constructor(id: string, central: Central, agent: Agent) {
+  private agentContext: AgentContext
+  private messenger: Ble
+
+  public constructor(id: string, messenger: Ble, agentContext: AgentContext) {
     this.id = id ?? utils.uuid()
-    this.agent = agent
-    this.central = central
+    this.messenger = messenger
+    this.agentContext = agentContext
   }
 
   public async send(agentContext: AgentContext, encryptedMessage: EncryptedMessage): Promise<void> {
     const serializedMessage = JSON.stringify(encryptedMessage)
 
-    this.agent.config.logger.debug('Sending BLE inbound message via session')
-    await this.central.sendMessage(serializedMessage)
+    agentContext.config.logger.debug('Sending BLE inbound message via session')
+    await this.messenger.sendMessage(serializedMessage)
   }
 
   public async close(): Promise<void> {
-    this.agent.config.logger.debug('Stopping BLE inbound session')
+    this.agentContext.config.logger.debug('Stopping BLE inbound session')
   }
 }
